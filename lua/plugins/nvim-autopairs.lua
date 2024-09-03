93e1fc15f10511baa71b74057b940ab70bc34263
pkj <pkjifinity@outlook.com>
2024-09-03 16:23:37 +0800

    [update]:autopairs plugin configs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -4,13 +4,13 @@ return {
   event = "InsertEnter",
   opts = function(_, opts)
     return require("astrocore").extend_tbl(opts, {
-      enable_check_bracket_line = true,
+      enable_check_bracket_line = false,
       map_c_h = true,
       map_c_w = true,
       map_bs = true,
       check_ts = true,
       enable_abbr = true,
-      map_cr = false,
+      map_cr = true,
     })
   end,
 }
